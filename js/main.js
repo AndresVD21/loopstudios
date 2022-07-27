70c9d1046625de9cf088e4e16f9dc559a4ea6ea1
Andres Valencia <andresvalencia.diaz@davita.com>
2022-07-27 13:06:59 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+menuBtn.addEventListener('click', () => {
+	menuBtn.classList.toggle('open');
+	menu.classList.toggle('hidden');
+	menu.classList.toggle('flex');
+});
